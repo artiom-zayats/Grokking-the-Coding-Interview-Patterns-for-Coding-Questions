fbd2e0ccb306b03835f9b61117de5cbd24a77b04
Tom Zayats <artiom.zayats@gmail.com>
2021-04-05 22:31:38 -0700

    update

--- 10. Pattern Subsets/Problem Challenge 1 - Evaluate Expression (hard).py
+++ 10. Pattern Subsets/Problem Challenge 1 - Evaluate Expression (hard).py
@@ -18,58 +18,43 @@ Input: "2*3-4-5"
 Output: 8, -12, 7, -7, -3 
 Explanation: 2*(3-(4-5)) => 8, 2*(3-4-5) => -12, 2*3-(4-5) => 7, 2*(3-4)-5 => -7, (2*3)-4-5 => -3
 '''
+
+import timeit
+
 #mycode2
 def diff_ways_to_evaluate_expression(input):
   result = []
   # TODO: Write your code here
+  if not input:
+    return []
 
+  if len(input) == 1:
+    return [input]
+  
+  for i in range(len(input)):
+    if input[i] in ["*","+","-"]:
+      res1 = diff_ways_to_evaluate_expression(input[:i])
+      res2 = diff_ways_to_evaluate_expression(input[i+1:])
+      if input[i] == "+":
+        temp = [int(x)+int(y) for x in res1 for y in res2]
+      if input[i] == "*":
+        temp = [int(x)*int(y) for x in res1 for y in res2]
+      if input[i] == "-":
+        temp = [int(x)-int(y) for x in res1 for y in res2]
 
-
+      result+= temp
   return result
 
 
 def main():
 
-  print("Expression evaluations: " +
+  print("Expression evaluations1: " +
         str(diff_ways_to_evaluate_expression("1+2*3")))
 
-  print("Expression evaluations: " +
+  print("Expression evaluations1: " +
         str(diff_ways_to_evaluate_expression("2*3-4-5")))
 
-
-main()
-
-
-#mycode
-def diff_ways_to_evaluate_expression(input):
-  result = []
-  # TODO: Write your code here
-
-  if '+' not in input and '-' not in input and '*' not in input:
-    result.append(input)
-  else:
-    for i in range(len(input)):
-      if not input[i].isdigit():
-        left = diff_ways_to_evaluate_expression(input[0:i])
-        right = diff_ways_to_evaluate_expression(input[i+1:])
-
-        for j in left:
-          for k in right:
-            if input[i] == '+':
-              result.append(int(j)+int(k))
-            if input[i] == '*':
-              result.append(int(j)*int(k))
-            if input[i] == '-':
-              result.append(int(j)-int(k))
-  return result
-
-
-def main():
-  print("Expression evaluations: " +
-        str(diff_ways_to_evaluate_expression("1+2*3")))
-
-  print("Expression evaluations: " +
-        str(diff_ways_to_evaluate_expression("2*3-4-5")))
+  
 
 
 main()
@@ -77,64 +62,101 @@ main()
 
 
 
-#answer
-def diff_ways_to_evaluate_expression(input):
-  return diff_ways_to_evaluate_expression_rec({}, input)
+# #mycode
+# def diff_ways_to_evaluate_expression(input):
+#   result = []
+#   # TODO: Write your code here
+
+#   if '+' not in input and '-' not in input and '*' not in input:
+#     result.append(input)
+#   else:
+#     for i in range(len(input)):
+#       if not input[i].isdigit():
+#         left = diff_ways_to_evaluate_expression(input[0:i])
+#         right = diff_ways_to_evaluate_expression(input[i+1:])
+
+#         for j in left:
+#           for k in right:
+#             if input[i] == '+':
+#               result.append(int(j)+int(k))
+#             if input[i] == '*':
+#               result.append(int(j)*int(k))
+#             if input[i] == '-':
+#               result.append(int(j)-int(k))
+#   return result
 
 
-def diff_ways_to_evaluate_expression_rec(map, input):
-  if input in map:
-    return map[input]
+# def main():
+#   print("Expression evaluations: " +
+#         str(diff_ways_to_evaluate_expression("1+2*3")))
 
-  result = []
-  # base case: if the input string is a number, parse and return it.
-  if '+' not in input and '-' not in input and '*' not in input:
-    result.append(int(input))
-  else:
-    for i in range(0, len(input)):
-      char = input[i]
-      if not char.isdigit():
-        # break the equation here into two parts and make recursively calls
-        leftParts = diff_ways_to_evaluate_expression_rec(
-          map, input[0:i])
-        rightParts = diff_ways_to_evaluate_expression_rec(
-          map, input[i+1:])
-        for part1 in leftParts:
-          for part2 in rightParts:
-            if char == '+':
-              result.append(part1 + part2)
-            elif char == '-':
-              result.append(part1 - part2)
-            elif char == '*':
-              result.append(part1 * part2)
-
-  map[input] = result
-  return result
+#   print("Expression evaluations: " +
+#         str(diff_ways_to_evaluate_expression("2*3-4-5")))
 
 
-def main():
-  print("Expression evaluations: " +
-        str(diff_ways_to_evaluate_expression("1+2*3")))
-
-  print("Expression evaluations: " +
-        str(diff_ways_to_evaluate_expression("2*3-4-5")))
-
-
-main()
+# #main()
 
 
 
-'''
-Time complexity 
-The time complexity of this algorithm will be exponential and will be similar to Balanced Parentheses. 
-Estimated time complexity will be O(N*2^N) but the actual time complexity ( O(4^n/\sqrt{n}) ) is bounded by the Catalan number and is beyond the scope of a coding interview.
 
-Space complexity 
-The space complexity of this algorithm will also be exponential, estimated at O(2^N) though the actual will be ( O(4^n/\sqrt{n})).
+# #answer
+# def diff_ways_to_evaluate_expression(input):
+#   return diff_ways_to_evaluate_expression_rec({}, input)
 
-Memoized version 
-The problem has overlapping subproblems, 
-as our recursive calls can be evaluating the same sub-expression multiple times. 
-To resolve this, we can use memoization and store the intermediate results in a HashMap. 
-In each function call, we can check our map to see if we have already evaluated this sub-expression before. 
-'''
+
+# def diff_ways_to_evaluate_expression_rec(map, input):
+#   if input in map:
+#     return map[input]
+
+#   result = []
+#   # base case: if the input string is a number, parse and return it.
+#   if '+' not in input and '-' not in input and '*' not in input:
+#     result.append(int(input))
+#   else:
+#     for i in range(0, len(input)):
+#       char = input[i]
+#       if not char.isdigit():
+#         # break the equation here into two parts and make recursively calls
+#         leftParts = diff_ways_to_evaluate_expression_rec(
+#           map, input[0:i])
+#         rightParts = diff_ways_to_evaluate_expression_rec(
+#           map, input[i+1:])
+#         for part1 in leftParts:
+#           for part2 in rightParts:
+#             if char == '+':
+#               result.append(part1 + part2)
+#             elif char == '-':
+#               result.append(part1 - part2)
+#             elif char == '*':
+#               result.append(part1 * part2)
+
+#   map[input] = result
+#   return result
+
+
+# def main():
+#   print("Expression evaluations: " +
+#         str(diff_ways_to_evaluate_expression("1+2*3")))
+
+#   print("Expression evaluations: " +
+#         str(diff_ways_to_evaluate_expression("2*3-4-5")))
+
+
+# #main()
+
+
+
+# '''
+# Time complexity 
+# The time complexity of this algorithm will be exponential and will be similar to Balanced Parentheses. 
+# Estimated time complexity will be O(N*2^N) but the actual time complexity ( O(4^n/\sqrt{n}) ) is bounded by the Catalan number and is beyond the scope of a coding interview.
+
+# Space complexity 
+# The space complexity of this algorithm will also be exponential, estimated at O(2^N) though the actual will be ( O(4^n/\sqrt{n})).
+
+# Memoized version 
+# The problem has overlapping subproblems, 
+# as our recursive calls can be evaluating the same sub-expression multiple times. 
+# To resolve this, we can use memoization and store the intermediate results in a HashMap. 
+# In each function call, we can check our map to see if we have already evaluated this sub-expression before. 
+# '''
